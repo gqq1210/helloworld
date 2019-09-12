0607bc29ff0e5a731c73fee868c2d028c7cb9208
gqq1210 <52596706+gqq1210@users.noreply.github.com>
2019-09-12 13:48:25 +0800

    Add files via upload
    Program listing1-1

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package welcome;
+
+public class Welcome {
+	public static void main(String[] args) {
+		// Display message Welcome to Java! on the console
+		System.out.println("Welcome to Java!");
+
+	}
+
+}
