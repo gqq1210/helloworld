19f93acc5bed13bcd44e7c8f5067c3ee8275da51
gqq1210 <52596706+gqq1210@users.noreply.github.com>
2019-09-24 16:27:08 +0800

    Add files via upload

---
+++ AnalyzeNumbers.java
@@ -0,0 +1,30 @@
+package program1;
+
+public class AnalyzeNumbers {
+
+	public static void main(String[] args) {
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		System.out.print("Enter the number of items: ");
+		int n = input.nextInt();
+		double [] numbers = new double[n];
+		double sum =0;
+		
+		System.out.print("Enter the numbers: ");
+		for(int i = 0;i<n;i++) {
+			numbers[i] = input.nextDouble();
+			sum +=numbers[i];
+		}
+		
+		double average = sum/n;
+		int count = 0;
+		for(int j =0;j<n;j++) {
+			if(numbers[j]>average)
+				count++;
+		}
+		
+		System.out.println("Average is " + average);
+		System.out.println("Number of elements above the average is " + count );
+		input.close();
+	}
+
+}
